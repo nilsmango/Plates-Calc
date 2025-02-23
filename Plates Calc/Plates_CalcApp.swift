2beba1c898d25f0d6f538a2b602e0e112b11c270
nilsmango <simonmilagrolang@icloud.com>
2025-02-23 20:22:19 +0100

    Initial Commit

---
+++ Plates Calc/Plates_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plates_CalcApp.swift
+//  Plates Calc
+//
+//  Created by Simon Lang on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Plates_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plates CalcTests/Plates_CalcTests.swift
@@ -0,0 +1,16 @@
+//
+//  Plates_CalcTests.swift
+//  Plates CalcTests
+//
+//  Created by Simon Lang on 23.02.2025.
+//
+
+import Testing
+
+struct Plates_CalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
